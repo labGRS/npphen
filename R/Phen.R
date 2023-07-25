07a4e4c15aa4dd63833fc37d086c9f083f9306ae
JoseLastra <jose.lastra@pucv.cl>
2023-07-25 10:29:05 -0400

    fix integer problem and rge error stoping kde

--- R/Phen.R
+++ R/Phen.R
@@ -15,42 +15,42 @@
 #' # Load data
 #' data("phents")
 #' # Phenology for the given data
-#' Phen(x=phents$NDVI,dates=phents$dates,h=2,frequency = '16-days',rge=c(0,10000))
+#' Phen(x = phents$NDVI, dates = phents$dates, h = 2, frequency = "16-days", rge = c(0, 10000))
 #' }
 #' \donttest{
 #' library(lubridate)
 #' library(terra)
 #' ## Testing raster data from Central Chile (NDVI), h=2##
 #' # Load data
-#' f <- system.file('extdata/MegaDrought_spatRast.rda', package = 'npphen')
+#' f <- system.file("extdata/MegaDrought_spatRast.rda", package = "npphen")
 #' MegaDrought <- readRDS(f)
-#' #Dates
+#' # Dates
 #' data("modis_dates")
 #'
 #' # Generate a Raster time series using a raster stack and a date database from Central Chile
 #' # Obtain data from a particular pixel generating a time series
-#' md_pixel <- cellFromXY(MegaDrought,cbind(313395,6356610))
+#' md_pixel <- cellFromXY(MegaDrought, cbind(313395, 6356610))
 #' md_pixelts <- as.numeric(MegaDrought[md_pixel])
-#' plot(modis_dates,md_pixelts, type='l')
+#' plot(modis_dates, md_pixelts, type = "l")
 #'
 #' # Phenology for the given pixel
-#' Phen(x=md_pixelts,dates=modis_dates,h=2,frequency='16-days',rge=c(0,10000))
+#' Phen(x = md_pixelts, dates = modis_dates, h = 2, frequency = "16-days", rge = c(0, 10000))
 #'
 #' ## Testing with the Bdesert_stack from the Atacama Desert, Northern Chile (NDVI), h=2 ##
 #'
 #' # Load data
-#' #SparRaster
-#' f <- system.file('extdata/Bdesert_spatRast.rda', package = 'npphen')
+#' # SparRaster
+#' f <- system.file("extdata/Bdesert_spatRast.rda", package = "npphen")
 #' Bdesert <- readRDS(f)
 #'
 #' # Generate a Raster time series using a raster stack and a date database from Northern Chile
 #' # Obtain data from a particular pixel generating a time series
-#' bd_pixel<-cellFromXY(Bdesert,cbind(286638,6852107))
-#' bd_pixelts<-as.numeric(Bdesert[bd_pixel])
-#' plot(modis_dates,bd_pixelts, type = 'l')
+#' bd_pixel <- cellFromXY(Bdesert, cbind(286638, 6852107))
+#' bd_pixelts <- as.numeric(Bdesert[bd_pixel])
+#' plot(modis_dates, bd_pixelts, type = "l")
 #'
 #' # Phenology for the given pixel
-#' Phen(x=bd_pixelts,dates=modis_dates,h=2,frequency='16-days',rge=c(0,10000))
+#' Phen(x = bd_pixelts, dates = modis_dates, h = 2, frequency = "16-days", rge = c(0, 10000))
 #' }
 #' @import ks
 #' @import grDevices
@@ -63,95 +63,115 @@
 #' @export
 
 Phen <-
-  function(x,dates,h,frequency = '16-days',rge) {
+  function(x, dates, h, frequency = "16-days", rge) {
+    if (length(rge) != 2) {
+      stop("rge must be a vector of length 2")
+    }
+    if (rge[1] > rge[2]) {
+      stop("rge vector order must be minimum/maximum")
+    }
+    if (length(dates) != length(x)) {
+      stop("N of dates and files do not match")
+    }
 
-    if(length(rge)!=2){stop("rge must be a vector of length 2")}
-    if(rge[1]>rge[2]){stop("rge vector order must be minimum/maximum")}
-    if(length(dates)!=length(x)){stop("N of dates and files do not match")}
-    
-    freq.method <- match(frequency, c("daily", "8-days", "16-days", 
-                               "bi-weekly", "monthly"))
-    if (is.na(freq.method)){ 
-      stop("Invalid frequency. Must be one of: daily, 8-days, 16-days, bi-weekly or monthly")}
-    if(frequency == 'daily'){
+    freq.method <- match(frequency, c(
+      "daily", "8-days", "16-days",
+      "bi-weekly", "monthly"
+    ))
+    if (is.na(freq.method)) {
+      stop("Invalid frequency. Must be one of: daily, 8-days, 16-days, bi-weekly or monthly")
+    }
+    if (frequency == "daily") {
       nGS <- 365
     }
-    if(frequency == '8-days'){
+    if (frequency == "8-days") {
       nGS <- 46
     }
-    if(frequency == '16-days'){
+    if (frequency == "16-days") {
       nGS <- 23
     }
-    if(frequency == 'monthly'){
+    if (frequency == "monthly") {
       nGS <- 12
     }
-    if(frequency == 'bi-monthly'){
+    if (frequency == "bi-monthly") {
       nGS <- 24
     }
     if (all(is.na(x))) {
-      return(rep(NA,nGS))
+      return(rep(NA, nGS))
     }
-    
-    DOY <- yday(dates)
-    DOY[which(DOY==366)]<-365
-    D1<-cbind(DOY,x)
-    if(length(unique(D1[,2]))<10 | (nrow(D1)-sum(is.na(D1)))<(0.1*nrow(D1))) {
-      return(rep(NA,nGS))
+    if (all(x < rge[1]) | all(x > rge[2], na.rm = T)) {
+      return(rep(NA, nGS))
     }
 
-    if(h!=1 && h!=2){stop("Invalid h")}
-    DOGS<-cbind(seq(1,365),c(seq(185,365),seq(1,184)))
-    if(h==2){
-      for(i in 1:nrow(D1)){
-        D1[i,1]<-DOGS[which(DOGS[,1]==D1[i,1],arr.ind=TRUE),2]}}
-    
-    Hmat<-Hpi(na.omit(D1))
-    Hmat[1,2]<-Hmat[2,1]
-    K1<-kde(na.omit(D1),H=Hmat,xmin=c(1,rge[1]),xmax=c(365,rge[2]),gridsize=c(365,500))
-    K1Con<-K1$estimate
-    
-    for(j in 1:365){ 
-      Kdiv<-sum(K1$estimate[j,])
-      ifelse(Kdiv==0,K1Con[j,]<-0,K1Con[j,]<-K1$estimate[j,]/sum(K1$estimate[j,]))}
-    
-    MAXY<-apply(K1Con,1,max)
-    for(i in 1:365){ 
-      n.select <- which(K1Con[i,]==MAXY[i],arr.ind=TRUE)
-      if(length(n.select) > 1){
-        n <- n.select[1]
-        MAXY[i]<-NA}
-      if(length(n.select) == 1){
-        n <- n.select
-        MAXY[i]<-median(K1$eval.points[[2]][n])
+    DOY <- yday(dates)
+    DOY[which(DOY == 366)] <- 365
+    D1 <- cbind(DOY, x)
+    if (length(unique(D1[, 2])) < 10 | (nrow(D1) - sum(is.na(D1))) < (0.1 * nrow(D1))) {
+      return(rep(NA, nGS))
+    }
+
+    if (h != 1 && h != 2) {
+      stop("Invalid h")
+    }
+    DOGS <- cbind(seq(1, 365), c(seq(185, 365), seq(1, 184)))
+    if (h == 2) {
+      for (i in 1:nrow(D1)) {
+        D1[i, 1] <- DOGS[which(DOGS[, 1] == D1[i, 1], arr.ind = TRUE), 2]
       }
     }
 
-    if(frequency == 'daily'){
-      select_DGS <- seq(1,365,1)
+    Hmat <- Hpi(na.omit(D1))
+    Hmat[1, 2] <- Hmat[2, 1]
+    K1 <- kde(na.omit(D1), H = Hmat, xmin = c(1, rge[1]), xmax = c(365, rge[2]), gridsize = c(365, 500))
+    K1Con <- K1$estimate
+
+    for (j in 1:365) {
+      Kdiv <- sum(K1$estimate[j, ])
+      ifelse(Kdiv == 0, K1Con[j, ] <- 0, K1Con[j, ] <- K1$estimate[j, ] / sum(K1$estimate[j, ]))
+    }
+
+    MAXY <- apply(K1Con, 1, max)
+    for (i in 1:365) {
+      n.select <- which(K1Con[i, ] == MAXY[i], arr.ind = TRUE)
+      if (length(n.select) > 1) {
+        n <- n.select[1]
+        MAXY[i] <- NA
+      }
+      if (length(n.select) == 1) {
+        n <- n.select
+        MAXY[i] <- median(K1$eval.points[[2]][n])
+      }
+    }
+
+    if (frequency == "daily") {
+      select_DGS <- seq(1, 365, 1)
       Ref <- MAXY
     }
-    if(frequency == '8-days'){
-      select_DGS <- seq(1,365,8)
+    if (frequency == "8-days") {
+      select_DGS <- seq(1, 365, 8)
       Ref <- MAXY[select_DGS]
     }
-    if(frequency == '16-days'){
-      select_DGS <- seq(1,365,16)
+    if (frequency == "16-days") {
+      select_DGS <- seq(1, 365, 16)
       Ref <- MAXY[select_DGS]
     }
-    if(frequency == 'monthly'){
-      select_DGS <- c(15,46,74,105,135,166,196,227,258,288,319,349)
+    if (frequency == "monthly") {
+      select_DGS <- c(15, 46, 74, 105, 135, 166, 196, 227, 258, 288, 319, 349)
       Ref <- MAXY[select_DGS]
     }
-    if(frequency == 'bi-weekly'){
-      select_DGS <- c(1,15,32,46,60,74,91,105,121,135,152,166,182,196,213,227,244,258,274,288,305,319,335,349)
+    if (frequency == "bi-weekly") {
+      select_DGS <- c(1, 15, 32, 46, 60, 74, 91, 105, 121, 135, 152, 166, 182, 196, 213, 227, 244, 258, 274, 288, 305, 319, 335, 349)
       Ref <- MAXY[select_DGS]
     }
-    if(h==1){id.label <- 'DOY'}
-    if(h==2){id.label <- 'DGS'}
-    plot(select_DGS,Ref,xlab=id.label,ylab='VI',font.lab=2,type='l') 
-    axis(1, at = seq(0,365,50))
-    
-    names(Ref) <- select_DGS 
+    if (h == 1) {
+      id.label <- "DOY"
+    }
+    if (h == 2) {
+      id.label <- "DGS"
+    }
+    plot(select_DGS, Ref, xlab = id.label, ylab = "VI", font.lab = 2, type = "l")
+    axis(1, at = seq(0, 365, 50))
+
+    names(Ref) <- select_DGS
     Ref
   }
-
--- R/PhenKplot.R
+++ R/PhenKplot.R
@@ -15,106 +15,123 @@
 #' # Load data
 #' data("phents")
 #' # PhenKplot for the given data
-#' PhenKplot(x=phents$NDVI,dates=phents$dates,h=2,
-#' xlab = "Day of growing season [DGS]",
-#' ylab = "NDVI",rge=c(0,10000))
+#' PhenKplot(
+#'   x = phents$NDVI, dates = phents$dates, h = 2,
+#'   xlab = "Day of growing season [DGS]",
+#'   ylab = "NDVI", rge = c(0, 10000)
+#' )
 #' }
 #' \donttest{
 #' library(lubridate)
 #' library(terra)
 #' ## Testing raster data from Central Chile (NDVI), h=2##
 #' # Load data
-#' f <- system.file('extdata/MegaDrought_spatRast.rda', package = 'npphen')
+#' f <- system.file("extdata/MegaDrought_spatRast.rda", package = "npphen")
 #' MegaDrought <- readRDS(f)
-#' #Dates
+#' # Dates
 #' data("modis_dates")
 #'
 #' # Generate a Raster time series using a raster stack and a date database from Central Chile
 #' # Obtain data from a particular pixel generating a time series
-#' md_pixel <- cellFromXY(MegaDrought,cbind(313395,6356610))
+#' md_pixel <- cellFromXY(MegaDrought, cbind(313395, 6356610))
 #' md_pixelts <- as.numeric(MegaDrought[md_pixel])
-#' plot(modis_dates,md_pixelts, type='l')
+#' plot(modis_dates, md_pixelts, type = "l")
 #'
 #' # Phenology for the given pixel
-#' PhenKplot(x=md_pixelts,dates=modis_dates,h=2,xlab="DGS", ylab="NDVI", rge=c(0,10000))
+#' PhenKplot(x = md_pixelts, dates = modis_dates, h = 2, xlab = "DGS", ylab = "NDVI", rge = c(0, 10000))
 #'
 #'
 #' ## Testing with the Bdesert_stack from the Atacama Desert, Northern Chile (NDVI), h=2 ##
 #'
 #' # Load data
-#' #SparRaster
-#' f <- system.file('extdata/Bdesert_spatRast.rda', package = 'npphen')
+#' # SparRaster
+#' f <- system.file("extdata/Bdesert_spatRast.rda", package = "npphen")
 #' Bdesert <- readRDS(f)
 #'
 #' # Generate a Raster time series using a raster stack and a date database from Northern Chile
 #' # Obtain data from a particular pixel generating a time series
-#' bd_pixel<-cellFromXY(Bdesert,cbind(286638,6852107))
-#' bd_pixelts<-as.numeric(Bdesert[bd_pixel])
-#' plot(modis_dates,bd_pixelts, type = 'l')
+#' bd_pixel <- cellFromXY(Bdesert, cbind(286638, 6852107))
+#' bd_pixelts <- as.numeric(Bdesert[bd_pixel])
+#' plot(modis_dates, bd_pixelts, type = "l")
 #'
 #' # Phenology for the given pixel
-#' PhenKplot(x=bd_pixelts,dates=modis_dates,h=2,xlab = 'DGS',ylab = 'NDVI',rge=c(0,10000))
-#'}
+#' PhenKplot(x = bd_pixelts, dates = modis_dates, h = 2, xlab = "DGS", ylab = "NDVI", rge = c(0, 10000))
+#' }
 #' @export
 
 PhenKplot <-
-  function(x,dates,h,xlab,ylab,rge) {
+  function(x, dates, h, xlab, ylab, rge) {
+    if (length(rge) != 2) {
+      stop("rge must be a vector of length 2")
+    }
+    if (rge[1] > rge[2]) {
+      stop("rge vector order must be minimum/maximum")
+    }
+    if (length(dates) != length(x)) {
+      stop("N of dates and files do not match")
+    }
 
-    if(length(rge)!=2){stop("rge must be a vector of length 2")}
-    if(rge[1]>rge[2]){stop("rge vector order must be minimum/maximum")}
-    if(length(dates)!=length(x)){stop("N of dates and files do not match")}
-  
     nGS <- 365
-   
+
     if (all(is.na(x))) {
-      return(rep(NA,nGS))
+      return(rep(NA, nGS))
     }
+
+    if (all(x < rge[1]) | all(x > rge[2], na.rm = T)) {
+      return(rep(NA, nGS))
+    }
+
     DOY <- lubridate::yday(dates)
-    DOY[which(DOY==366)]<-365
-    D1<-cbind(DOY,x)
-    if(length(unique(D1[,2]))<10 | (nrow(D1)-sum(is.na(D1)))<(0.1*nrow(D1))) { 
-      return(rep(NA,nGS))
+    DOY[which(DOY == 366)] <- 365
+    D1 <- cbind(DOY, x)
+    if (length(unique(D1[, 2])) < 10 | (nrow(D1) - sum(is.na(D1))) < (0.1 * nrow(D1))) {
+      return(rep(NA, nGS))
     }
 
-    if(h!=1 && h!=2){stop("Invalid h")}
-    DOGS<-cbind(seq(1,365),c(seq(185,365),seq(1,184)))
-    if(h==2){
-      for(i in 1:nrow(D1)){
-        D1[i,1]<-DOGS[which(DOGS[,1]==D1[i,1],arr.ind=TRUE),2]}}
+    if (h != 1 && h != 2) {
+      stop("Invalid h")
+    }
+    DOGS <- cbind(seq(1, 365), c(seq(185, 365), seq(1, 184)))
+    if (h == 2) {
+      for (i in 1:nrow(D1)) {
+        D1[i, 1] <- DOGS[which(DOGS[, 1] == D1[i, 1], arr.ind = TRUE), 2]
+      }
+    }
 
-    Hmat<-ks::Hpi(na.omit(D1))
-    Hmat[1,2]<-Hmat[2,1]
-    K1<-ks::kde(na.omit(D1),H=Hmat,xmin=c(1,rge[1]),xmax=c(365,rge[2]),gridsize=c(365,500))
-    K1Con<-K1$estimate
-    
-    for(j in 1:365){ 
-      Kdiv<-sum(K1$estimate[j,])
-      ifelse(Kdiv==0,K1Con[j,]<-0,K1Con[j,]<-K1$estimate[j,]/sum(K1$estimate[j,]))}
-    
-    MAXY<-apply(K1Con,1,max)
-    for(i in 1:365){ 
-      n.select <- which(K1Con[i,]==MAXY[i],arr.ind=TRUE)
-      if(length(n.select) > 1){
+    Hmat <- ks::Hpi(na.omit(D1))
+    Hmat[1, 2] <- Hmat[2, 1]
+    K1 <- ks::kde(na.omit(D1), H = Hmat, xmin = c(1, rge[1]), xmax = c(365, rge[2]), gridsize = c(365, 500))
+    K1Con <- K1$estimate
+
+    for (j in 1:365) {
+      Kdiv <- sum(K1$estimate[j, ])
+      ifelse(Kdiv == 0, K1Con[j, ] <- 0, K1Con[j, ] <- K1$estimate[j, ] / sum(K1$estimate[j, ]))
+    }
+
+    MAXY <- apply(K1Con, 1, max)
+    for (i in 1:365) {
+      n.select <- which(K1Con[i, ] == MAXY[i], arr.ind = TRUE)
+      if (length(n.select) > 1) {
         n <- n.select[1]
-        MAXY[i]<-NA}
-      if(length(n.select) == 1){
+        MAXY[i] <- NA
+      }
+      if (length(n.select) == 1) {
         n <- n.select
-        MAXY[i]<-median(K1$eval.points[[2]][n])
+        MAXY[i] <- median(K1$eval.points[[2]][n])
       }
     }
 
     h2d <- list()
-    h2d$x <- seq(1,365)
-    h2d$y <- seq(rge[1],rge[2],len=500)
-    h2d$density <- K1Con/sum(K1Con)
+    h2d$x <- seq(1, 365)
+    h2d$y <- seq(rge[1], rge[2], len = 500)
+    h2d$density <- K1Con / sum(K1Con)
     uniqueVals <- rev(unique(sort(h2d$density)))
     cumProbs <- cumsum(uniqueVals)
     names(cumProbs) <- uniqueVals
     h2d$cumDensity <- matrix(nrow = nrow(h2d$density), ncol = ncol(h2d$density))
     h2d$cumDensity[] <- cumProbs[as.character(h2d$density)]
 
-    image(h2d$x, h2d$y, h2d$cumDensity, xlab=xlab, ylab=ylab, font.lab=2, breaks = c(0,0.5, 0.75, 0.9,0.95),col=grDevices::heat.colors(n=4,alpha=0.6))
-    contour(h2d$x, h2d$y, h2d$cumDensity, levels = c(0,0.5, 0.75, 0.9,0.95),add=T,col=grDevices::grey(0.25),labcex=1)
-    lines(seq(1,365),MAXY,lwd=3,col='dark red')
-}
-
+    image(h2d$x, h2d$y, h2d$cumDensity, xlab = xlab, ylab = ylab, font.lab = 2, breaks = c(0, 0.5, 0.75, 0.9, 0.95), col = grDevices::heat.colors(n = 4, alpha = 0.6))
+    contour(h2d$x, h2d$y, h2d$cumDensity, levels = c(0, 0.5, 0.75, 0.9, 0.95), add = T, col = grDevices::grey(0.25), labcex = 1)
+    lines(seq(1, 365), MAXY, lwd = 3, col = "dark red")
+  }
