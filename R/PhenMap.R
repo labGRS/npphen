c6eee05b2b8114a7e6e02f40968a550579ba44c0
JoseLastra <jose.lastra@pucv.cl>
2025-05-09 14:27:03 +0200

    check finished

--- R/PhenMap.R
+++ R/PhenMap.R
@@ -92,14 +92,14 @@ PhenMap <-
       if (all(is.na(x))) {
         return(rep(NA, nGS))
       }
-      if (all(x < rge[1], na.rm = T) | all(x > rge[2], na.rm = T)) {
+      if (all(x < rge[1], na.rm = T) || all(x > rge[2], na.rm = T)) {
         return(rep(NA, nGS))
       }
 
       DOY <- lubridate::yday(dates)
       DOY[which(DOY == 366)] <- 365L
       D1 <- cbind(DOY, x)
-      if (length(unique(D1[, 2])) < 10 | (nrow(D1) - sum(is.na(D1))) < (0.1 * nrow(D1))) {
+      if (length(unique(D1[, 2])) < 10 || (nrow(D1) - sum(is.na(D1))) < (0.1 * nrow(D1))) {
         return(rep(NA, nGS))
       }
 
@@ -108,9 +108,7 @@ PhenMap <-
       }
       DOGS <- cbind(seq(1, 365), c(seq(185, 365), seq(1, 184)))
       if (h == 2) {
-        for (i in 1:nrow(D1)) {
           D1[, 1] <- DOGS[match(D1[, 1], DOGS[, 1]), 2]
-        }
       }
 
       Hmat <- ks::Hpi(na.omit(D1))
